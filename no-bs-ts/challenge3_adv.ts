746566f39d3a63dc97f9a55391948d0edf74e9b5
Sebastian F <sebpfdev@gmail.com>
2022-07-10 12:35:20 -0400

    Perform no-bs-ts challenge 3 advanced

--- no-bs-ts/challenge3_adv.ts
+++ no-bs-ts/challenge3_adv.ts
@@ -1,45 +1,104 @@
-class EventProcessor {
-    handleEvent(eventName: ..., data: ...): void {
+interface ProcessedEvent<T> {
+  eventName: keyof T;
+  data: T[keyof T];
+}
+
+type FilterFunction<T> = (val: T[keyof T]) => boolean;
+// type Filters<T> = Record<keyof T, FilterFunction<T>[]>;
+
+type MapFunction<T> = (val: T[keyof T]) => T[keyof T];
+// type Maps<T> = Record<keyof T, MapFunction<T>[]>;
+
+type Handler<T> = {
+  [Property in keyof T as `filter${Capitalize<string & Property>}`]?: (
+    val: T[Property]
+  ) => boolean;
+} & {
+  [Property in keyof T as `map${Capitalize<string & Property>}`]?: (
+    val: T[Property]
+  ) => T[Property];
+};
+
+const capitalize = (s: string) => `${s.charAt(0).toUpperCase()}${s.slice(1)}`;
+
+class EventProcessor<EvtMap> {
+  //   private filters: Filters<EvtMap> = {} as Filters<EvtMap>;
+  //   private maps: Maps<EvtMap> = {} as Maps<EvtMap>;
+  private handlers: Handler<EvtMap>[] = [];
+  private processedEvents: ProcessedEvent<EvtMap>[] = [];
+
+  handleEvent<EvtKey extends keyof EvtMap>(
+    eventName: EvtKey,
+    data: EvtMap[EvtKey]
+  ): void {
+    let validEvent = true;
+    for (const handler of this.handlers) {
+      const filterFunc = handler[
+        `filter${capitalize(eventName as string)}` as keyof Handler<EvtMap>
+      ] as unknown as ((val: EvtMap[EvtKey]) => boolean) | undefined;
+
+      if (filterFunc && !filterFunc(data)) {
+        validEvent = false;
+        break;
+      }
     }
-  
-    addHandler(handler: ...) {
+
+    if (!validEvent) return;
+
+    let mappedData = { ...data };
+    for (const handler of this.handlers) {
+      const mapFunc = handler[
+        `map${capitalize(eventName as string)}` as keyof Handler<EvtMap>
+      ] as unknown as ((val: EvtMap[EvtKey]) => EvtMap[EvtKey]) | undefined;
+
+      if (mapFunc) {
+        mappedData = mapFunc(mappedData);
+      }
     }
-  
-    getProcessedEvents(): ...[] {
-    }
+
+    this.processedEvents.push({ eventName, data: mappedData });
   }
-  
-  interface EventMap {
-    login: { user?: string; name?: string; hasSession?: boolean };
-    logout: { user?: string };
+
+  addHandler(handler: Handler<EvtMap>): void {
+    this.handlers.push(handler);
   }
-  
-  class UserEventProcessor extends EventProcessor<EventMap> {}
-  
-  const uep = new UserEventProcessor();
-  uep.addHandler({
-    filterLogin: ({ user }) => Boolean(user),
-    mapLogin: (data) => ({
-      ...data,
-      hasSession: Boolean(data.user && data.name),
-    }),
-  });
-  
-  uep.handleEvent("login", {
-    user: null,
-    name: "jack",
-  });
-  uep.handleEvent("login", {
-    user: "tom",
-    name: "tomas",
-  });
-  uep.handleEvent("logout", {
-    user: "tom",
-  });
-  
-  console.log(uep.getProcessedEvents());
-  
-  /*
+
+  getProcessedEvents(): ProcessedEvent<EvtMap>[] {
+    return this.processedEvents;
+  }
+}
+
+interface EventMap {
+  login: { user?: string; name?: string; hasSession?: boolean };
+  logout: { user?: string };
+}
+
+class UserEventProcessor extends EventProcessor<EventMap> {}
+
+const uep = new UserEventProcessor();
+uep.addHandler({
+  filterLogin: ({ user }) => Boolean(user),
+  mapLogin: (data) => ({
+    ...data,
+    hasSession: Boolean(data.user && data.name),
+  }),
+});
+
+uep.handleEvent('login', {
+  user: undefined,
+  name: 'jack',
+});
+uep.handleEvent('login', {
+  user: 'tom',
+  name: 'tomas',
+});
+uep.handleEvent('logout', {
+  user: 'tom',
+});
+
+console.log(uep.getProcessedEvents());
+
+/*
   Result:
   
   [
